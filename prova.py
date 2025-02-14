c34ecd2289dc1b220bb2f851fe33d5a8ad2b8e02
santillan-arturo -arturo <santillan.arturo@iisgalvanimi.edu.it>
2025-02-14 12:01:00 +0000

    quarto push

--- prova.py
+++ prova.py
@@ -1,3 +1,10 @@
 print(0)
 print('secondo commit')
-sgvchj
+yuhegyudgdsyu
+
+inventario = {
+
+0 : {"nome": "Il PC di ...", "quantità": 1, "prezzo": 3999},
+1 : {"nome": "Nintendo Switch 2", "quantità": 10, "prezzo": 449},
+2 : {"nome": "GTA VI", "quantità": 3, "prezzo": 100}
+}
